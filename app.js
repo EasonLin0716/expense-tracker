9e21f49d9a773e9e8c110f180338bc13596caaa1
Eason Lin <uu9924079@gmail.com>
2019-09-16 16:52:55 +0800

    Add Mongoose connection and create Record model

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const Record = require('./models/record')
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('index is here')
