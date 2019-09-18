63bb9668c0b191501625cce750fe81907ae8a04d
Eason Lin <uu9924079@gmail.com>
2019-09-18 15:33:12 +0800

    seeder example user and records updated

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
+const User = require('../user')
 const Record = require('../record')
+const userJson = require('./user.json')
 const expenseTrackerJson = require('./expenseTracker')
+const bcrypt = require('bcryptjs')
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -10,21 +13,51 @@ db.on('error', () => {
   console.log('mongodb error!')
 })
 
+
 db.once('open', () => {
-  console.log('mongodb connected!')
-  seedCreate()
-})
+  console.log('db connected!')
 
+  User.create(userJson).then(() =>
 
-function seedCreate() {
-  let count = 0
-  expenseTrackerJson.forEach(record => {
-    Record.create(record, () => {
-      count++
-      console.log(`${count}/${expenseTrackerJson.length} done`)
-      if (count === expenseTrackerJson.length) {
-        console.log('All set, Ready to go! (ctrl+c to quit)')
-      }
+    User.find({ email: 'asiagodtone@example.com' }, (err, user) => {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user[0].password, salt, (err, hash) => {
+          if (err) throw err
+          user[0].password = hash
+          user[0].save()
+        })
+      })
+
+      expenseTrackerJson[0]["userId"] = user[0]._id.toString()
+      expenseTrackerJson[1]["userId"] = user[0]._id.toString()
+      expenseTrackerJson[2]["userId"] = user[0]._id.toString()
+      expenseTrackerJson[3]["userId"] = user[0]._id.toString()
+      expenseTrackerJson[4]["userId"] = user[0]._id.toString()
     })
-  })
-}
+
+  ).then(() =>
+
+    User.find({ email: 'bigdaddy@example.com' }, (err, user) => {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user[0].password, salt, (err, hash) => {
+          if (err) throw err
+          user[0].password = hash
+          user[0].save()
+        })
+      })
+
+      expenseTrackerJson[5]["userId"] = user[0]._id.toString()
+      expenseTrackerJson[6]["userId"] = user[0]._id.toString()
+      expenseTrackerJson[7]["userId"] = user[0]._id.toString()
+      expenseTrackerJson[8]["userId"] = user[0]._id.toString()
+      expenseTrackerJson[9]["userId"] = user[0]._id.toString()
+    })
+
+  ).then(() =>
+
+    Record.create(expenseTrackerJson)
+
+  )
+
+  console.log('Data create complete!')
+})
