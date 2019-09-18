28e5cd9588afcf90d7dbe9db53970007fa3e7e92
Eason Lin <uu9924079@gmail.com>
2019-09-18 12:47:53 +0800

    add authenticated to home router

--- routes/home.js
+++ routes/home.js
@@ -3,9 +3,12 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../models/record')
 const sumAmount = require('../public/javascripts/sumAmount')
+const { authenticated } = require('../config/auth')
+
+
 
 // 首頁
-router.get('/', (req, res) => {
+router.get('/', authenticated, (req, res) => {
   /* declare selectedCategory as query of Record.find, if select button is clicked by user new query will be passed into if statement */
   const selectedCategory = {}
   if (req.query.category) selectedCategory.category = req.query.category
