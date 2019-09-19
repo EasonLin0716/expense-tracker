fdc2654f919f85035c58ffe648c4350bebd7dea3
Eason Lin <uu9924079@gmail.com>
2019-09-19 14:45:01 +0800

    add month filter feature

--- routes/home.js
+++ routes/home.js
@@ -10,8 +10,12 @@ const { authenticated } = require('../config/auth')
 // 首頁
 router.get('/', authenticated, (req, res) => {
   /* declare selectedCategory as query of Record.find, if select button is clicked by user new query will be passed into if statement */
+
+  // .find({ date: {$regex:/09/} })
+
   const selectedCategory = {}
   if (req.query.category) selectedCategory.category = req.query.category
+  if (req.query.month) selectedCategory.date = { $regex: `${req.query.month}` }
   console.log(selectedCategory)
   Record.find({ userId: req.user._id })
     .find(selectedCategory)
