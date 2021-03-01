386425c52b6ef63c40a8d99c366190d18190f3a7
Eason Lin <uu9924079@gmail.com>
2021-03-01 17:27:56 +0800

    add docker configs

--- app.js
+++ app.js
@@ -1,65 +1,70 @@
 // ./app.js
 /* -----require needed middlewares and others----- */
-const express = require('express')
-const app = express()
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+const express = require("express");
+const app = express();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-const exphbs = require('express-handlebars')
-const passport = require('passport')
-const session = require('express-session')
-const flash = require('connect-flash')
-
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 /* -----db connecting----- */
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-const Record = require('./models/record')
-const bodyParser = require('body-parser')
-
+if (typeof process.env.MONGODB_URI === "undefined") {
+  console.log("WARNING: env not setting MONGODB_URI");
+}
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/expense-tracker",
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+);
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+const Record = require("./models/record");
+const bodyParser = require("body-parser");
 
 /* -----middleware setting----- */
-app.use(express.static('public'))
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-const Handlebars = require("handlebars")
-Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
-  return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
-})
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-app.use(session({
-  secret: 'your secret key',
-  resave: false,
-  saveUninitialized: true,
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-require('./config/passport')(passport)
-app.use(flash())
+app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+const Handlebars = require("handlebars");
+Handlebars.registerHelper("ifEquals", function (arg1, arg2, options) {
+  return arg1 === arg2 ? options.fn(this) : options.inverse(this);
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "your secret key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport")(passport);
+app.use(flash());
 app.use((req, res, next) => {
-  res.locals.user = req.user
-  res.locals.isAuthenticated = req.isAuthenticated()
-  res.locals.success_msg = req.flash('success_msg')
-  res.locals.warning_msg = req.flash('warning_msg')
-  next()
-})
-
+  res.locals.user = req.user;
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.warning_msg = req.flash("warning_msg");
+  next();
+});
 
 /* -----route setting----- */
-app.use('/', require('./routes/home'))
-app.use('/records', require('./routes/record'))
-app.use('/users', require('./routes/user'))
-app.use('/auth', require('./routes/auths'))
+app.use("/", require("./routes/home"));
+app.use("/records", require("./routes/record"));
+app.use("/users", require("./routes/user"));
+app.use("/auth", require("./routes/auths"));
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('app.js is running')
-})
+  console.log("app.js is running");
+});
